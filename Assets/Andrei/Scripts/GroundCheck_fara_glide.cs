6b0a128e24420f761c29885484c95c25ea60e57b
Andrei Buscan <andreibuscan@yahoo.com>
2024-03-08 23:04:40 +0200

    fixed movement 2

---
+++ Assets/Andrei/Scripts/GroundCheck_fara_glide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck_fara_glide : MonoBehaviour
+{
+    [SerializeField]
+    float groundCheckLenght = 0.5f;
+    
+    private void FixedUpdate()
+    {
+        Movement movement = GetComponentInParent<Movement>();
+       movement.isGrounded = Physics.Raycast(transform.position, -transform.up, groundCheckLenght);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, -transform.up * groundCheckLenght);
+    }
+}
